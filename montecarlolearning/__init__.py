6e87300f7aee38ccef40d7a4c6e33e20ccc5779a
Daniel Roth <118249021+da-roth@users.noreply.github.com>
2022-12-20 13:30:46 +0100

    test update init

--- montecarlolearning/__init__.py
+++ montecarlolearning/__init__.py
@@ -1,4 +1,15 @@
+import numpy as np
+from scipy.stats import norm
+
+
 __version__ = 'dev'
 
 def hello_world():
     print("This is my first pip package!")
+    
+
+# helper analytics    
+def bsPrice(spot, strike, vol, T):
+    d1 = (np.log(spot/strike) + 0.5 * vol * vol * T) / vol / np.sqrt(T)
+    d2 = d1 - vol * np.sqrt(T)
+    return spot * norm.cdf(d1) - strike * norm.cdf(d2)    
