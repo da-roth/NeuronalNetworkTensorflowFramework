bug fix wrong b.s. formula

montecarlolearning/GBM.py
@@ -60,14 +60,14 @@ class GBM:
 
 
         if (self._opt == GBM_Case.ClosedSolutionAddtiveNoise):
-            d1 = (np.log(s_0[:]/K) + 0.5 * sigma * sigma * T) / sigma / np.sqrt(T)
+            d1 = (np.log(s_0[:]/K) + (mu + 0.5 * sigma * sigma) * T) / sigma / np.sqrt(T)
             d2 = d1[:] - sigma * np.sqrt(T)
             z=np.random.normal(0.0, self._noiseVariance, m)
             noisedPrice = s_0[:] * norm.cdf(d1[:]) - np.exp(-mu*T) * K * norm.cdf(d2[:]) + z[:]
             return s_0.reshape([-1,1]), noisedPrice.reshape([-1,1]), None
         
         elif (self._opt == GBM_Case.ClosedSolution):
-            d1 = (np.log(s_0[:]/K) + 0.5 * sigma * sigma * T) / sigma / np.sqrt(T)
+            d1 = (np.log(s_0[:]/K) +(mu + 0.5 * sigma * sigma) * T) / sigma / np.sqrt(T)
             d2 = d1[:] - sigma * np.sqrt(T)
             price = s_0[:] * norm.cdf(d1[:]) - np.exp(-mu*T) * K * norm.cdf(d2[:])
             return s_0.reshape([-1,1]), price.reshape([-1,1]), None
@@ -111,7 +111,7 @@ class GBM:
         K = 110
         
         # B.S. formula
-        d1 = (np.log(s_0[:]/K) + 0.5 * sigma * sigma * T) / sigma / np.sqrt(T)
+        d1 = (np.log(s_0[:]/K) + (mu + 0.5 * sigma * sigma) * T) / sigma / np.sqrt(T)
         d2 = d1[:] - sigma * np.sqrt(T)
         price = s_0[:] * norm.cdf(d1[:]) - np.exp(-mu*T) * K * norm.cdf(d2[:])
         return s_0.reshape([-1,1]), price.reshape([-1,1]), None, None

montecarlolearning/GBM_5d.py
@@ -86,14 +86,14 @@ class GBM_5d(TrainingDataGenerator):
 
         
         if (self._opt == GBM_5d_Case.ClosedSolutionAddtiveNoise):
-            d1 = (np.log(s_0[:]/K[:]) + 0.5 * sigma[:] * sigma[:] * T[:]) / sigma[:] / np.sqrt(T[:])
+            d1 = (np.log(s_0[:]/K[:]) + (mu[:] + 0.5 * sigma[:] * sigma[:]) * T[:]) / (sigma[:] * np.sqrt(T[:]))
             d2 = d1[:] - sigma[:] * np.sqrt(T[:])
             z=np.random.normal(0.0, self._noiseVariance, m)
             noisedPrice = s_0[:] * norm.cdf(d1[:]) - np.exp(-mu[:]*T[:]) * K[:] * norm.cdf(d2[:]) + z[:]
             return np.stack((s_0,sigma,mu,T,K),axis=1), noisedPrice.reshape([-1,1]), None
         
         elif (self._opt == GBM_5d_Case.ClosedSolution):
-            d1 = (np.log(s_0[:]/K[:]) + 0.5 * sigma[:] * sigma[:] * T[:]) / sigma[:] / np.sqrt(T[:])
+            d1 = (np.log(s_0[:]/K[:]) + (mu[:] + 0.5 * sigma[:] * sigma[:]) * T[:]) / (sigma[:] * np.sqrt(T[:]))
             d2 = d1[:] - sigma[:] * np.sqrt(T[:])
             price = s_0[:] * norm.cdf(d1[:]) - np.exp(-mu[:] *T[:] ) * K[:] * norm.cdf(d2[:])
             return np.stack((s_0,sigma,mu,T,K),axis=1), price.reshape([-1,1]), None
@@ -141,7 +141,7 @@ class GBM_5d(TrainingDataGenerator):
         K = (K_r - K_l) * np.random.random_sample(num) + K_l
         
         # B.S. formula
-        d1 = (np.log(s_0[:]/K[:]) + 0.5 * sigma[:] * sigma[:] * T[:]) / sigma[:] / np.sqrt(T[:])
+        d1 = (np.log(s_0[:]/K[:]) + (mu[:] + 0.5 * sigma[:] * sigma[:]) * T[:]) / (sigma[:] * np.sqrt(T[:]))
         d2 = d1[:] - sigma[:] * np.sqrt(T[:])
         price = s_0[:] * norm.cdf(d1[:]) - np.exp(-mu[:] * T[:]) * K[:] * norm.cdf(d2[:])
         return np.stack((s_0,sigma,mu,T,K),axis=1), price.reshape([-1,1]), None, None

montecarlolearning/Multilevel_GBM.py
@@ -15,7 +15,7 @@ class Multilevel_Train_Case(Enum):
 # main class
 class Multilevel_GBM(TrainingDataGenerator):
     
-    def __init__(self, opt=Multilevel_Train_Case.BS_Solution, steps = 1.0):
+    def __init__(self, opt=Multilevel_Train_Case.BS_Solution, steps = 1):
         
         # Call the parent class's constructor using super()
         super().__init__()
@@ -27,19 +27,34 @@ class Multilevel_GBM(TrainingDataGenerator):
         self._opt = opt # Case
         self._steps = steps     # discretization steps (if discretization is used)
         
+        # # Training set definition
+        # self.s_0_trainInterval = [118.0, 120.0]
+        # self.sigma_trainInterval = [0.1, 0.2]
+        # self.mu_trainInterval = [0.02, 0.05]
+        # self.T_trainInterval = [0.9, 1.0]
+        # self.K_trainInterval = [109.0, 110.0]
+        
+        # # Test set modification: (reducing test interval slightly for better testing)
+        # self.s_0_h = 0.4
+        # self.sigma_h = 0.01
+        # self.mu_h = 0.01  
+        # self.T_h = 0.01              
+        # self.K_h = 0.1
+
+        # 1d for testing
         # Training set definition
         self.s_0_trainInterval = [118.0, 120.0]
-        self.sigma_trainInterval = [0.1, 0.2]
-        self.mu_trainInterval = [0.02, 0.05]
-        self.T_trainInterval = [0.9, 1.0]
-        self.K_trainInterval = [109.0, 110.0]
+        self.sigma_trainInterval = [0.2, 0.2]
+        self.mu_trainInterval = [0.05, 0.05]
+        self.T_trainInterval = [10.0, 10.0]
+        self.K_trainInterval = [410.0, 410.0]
         
         # Test set modification: (reducing test interval slightly for better testing)
-        self.s_0_h = 0.4
-        self.sigma_h = 0.01
-        self.mu_h = 0.01  
-        self.T_h = 0.01              
-        self.K_h = 0.1
+        self.s_0_h = 0.0
+        self.sigma_h = 0.0
+        self.mu_h = 0.0
+        self.T_h = 0.0              
+        self.K_h = 0.0
         
     def trainingSet(self, m, trainSeed=None, approx=False):  
         #np.random.seed(trainSeed) 
@@ -53,13 +68,18 @@ class Multilevel_GBM(TrainingDataGenerator):
         if (self._opt == Multilevel_Train_Case.Milstein):
  
             # 2. Compute paths
+            
             h = T[:]/ self._steps
-            z=np.random.normal(0.0, 1.0, m)
-            s= s_0[:] + mu[:] *s_0[:] * h[:] +sigma[:] * s_0[:] *np.sqrt(h[:])*z[:] + 0.5 *sigma[:] *s_0[:] *sigma[:] * ((np.sqrt(h[:])*z[:])**2-h[:]) 
+            s = s_0
+            # loop through the array for 10 steps
+            for i in range(self._steps):
+                # do something with the array
+                z=np.random.normal(0.0, 1.0, m)
+                s= s[:] + mu[:] *s[:] * h[:] +sigma[:] * s[:] *np.sqrt(h[:])*z[:] + 0.5 *sigma[:] *s_0[:] *sigma[:] * ((np.sqrt(h[:])*z[:])**2-h[:]) 
             # 3. Calculate and return payoffs
             payoffs=np.exp(-mu[:] * T[:])* np.maximum(s[:] - K[:], 0.)
             return np.stack((s_0,sigma,mu,T,K),axis=1), payoffs.reshape([-1,1]), None
-        if (self._opt == Multilevel_Train_Case.GBM_Path_Solution):
+        elif (self._opt == Multilevel_Train_Case.GBM_Path_Solution):
             #3. sets of random returns
             h=T[:]
             z=np.random.normal(0.0,1.0,m)
@@ -69,13 +89,13 @@ class Multilevel_GBM(TrainingDataGenerator):
             return np.stack((s_0,sigma,mu,T,K),axis=1), payoffs.reshape([-1,1]), None
         else:
             # B.S. formula
-            d1 = (np.log(s_0[:]/K[:]) + 0.5 * sigma[:] * sigma[:] * T[:]) / sigma[:] / np.sqrt(T[:])
+            d1 = (np.log(s_0[:]/K[:]) + (mu[:] + 0.5 * sigma[:] * sigma[:]) * T[:]) / (sigma[:] * np.sqrt(T[:]))
             d2 = d1[:] - sigma[:] * np.sqrt(T[:])
             price = s_0[:] * norm.cdf(d1[:]) - np.exp(-mu[:] *T[:] ) * K[:] * norm.cdf(d2[:])
             return np.stack((s_0,sigma,mu,T,K),axis=1), price.reshape([-1,1]), None
      
     def testSet(self, num, testSeed=None):
-        np.random.seed(testSeed)
+        #np.random.seed(testSeed)
         # 0. Test interval definition
         s_0_testInterval = [self.s_0_trainInterval[0]+self.s_0_h, self.s_0_trainInterval[1]-self.s_0_h]
         sigma_testInterval = [self.sigma_trainInterval[0]+self.sigma_h, self.sigma_trainInterval[1]-self.sigma_h]
@@ -91,7 +111,7 @@ class Multilevel_GBM(TrainingDataGenerator):
         K = (K_testInterval[1] - K_testInterval[0]) * np.random.random_sample(num) + K_testInterval[0]
         
         # B.S. formula
-        d1 = (np.log(s_0[:]/K[:]) + 0.5 * sigma[:] * sigma[:] * T[:]) / sigma[:] / np.sqrt(T[:])
+        d1 = (np.log(s_0[:]/K[:]) + (mu[:] + 0.5 * sigma[:] * sigma[:]) * T[:]) / (sigma[:] * np.sqrt(T[:]))
         d2 = d1[:] - sigma[:] * np.sqrt(T[:])
         price = s_0[:] * norm.cdf(d1[:]) - np.exp(-mu[:] * T[:]) * K[:] * norm.cdf(d2[:])
         return np.stack((s_0,sigma,mu,T,K),axis=1), price.reshape([-1,1]), None, None

src/DebugArea/Main.py
@@ -8,24 +8,14 @@ import os
 mainDirectory = os.getcwd()
 packageFile = os.path.abspath(os.path.join(mainDirectory, 'montecarlolearning', 'packages.py'))
 exec(open(packageFile).read())
-### - dataSeed = seed for simulations or (for csv input) for randomization of csv
-dataSeed = 1 
-weightSeed = 1 
-
 
 ###
 ### 1. Training data
 ###
 #from CDF import *
-#sizes = [1000,100] # [sizePerTrainingStep, trainingSteps]
-#nTest = 2000 # Test set size
-
-Generator = CDF()
-#print(Generator.path == None)
-Generator.set_inputName('x')
-Generator.set_outputName('CDF(x)')
-Generator.set_trainingSetSizes([1000,100])
-Generator.set_nTest(2000)
+Generator = Multilevel_GBM(Multilevel_Train_Case.GBM_Path_Solution )
+Generator.set_inputName('S')
+Generator.set_outputName('EuropeanCallPrice(S)')
 
 ###
 ### 2. Set Nueral network structure / Hyperparameters
@@ -34,20 +24,23 @@ Generator.set_nTest(2000)
 Regressor = Neural_Approximator()
 Regressor.set_Generator(Generator)
 Regressor.set_hiddenNeurons(20)
-Regressor.set_hiddenLayers(3)
-Regressor.set_activationFunctionsHidden([tf.nn.tanh])
-
-#hiddenNeurons = 20               # we use equal neurons for each hidden layer
-#hiddenLayers = 3                # amount of hidden layers
-#activationFunctionsHidden = [tf.nn.tanh]   # activation functions of hidden layers
+Regressor.set_hiddenLayers(2)
+Regressor.set_activationFunctionsHidden(tf.nn.sigmoid)
+Regressor.set_activationFunctionOutput(tf.nn.sigmoid)
+Regressor.set_weight_seed(1)
 
 TrainSettings = TrainingSettings()
-TrainSettings.set_epochs(20)
+TrainSettings.set_learning_rate_schedule=([(0.0, 0.0001),  (0.2, 0.0001),  (0.4, 0.0001), (0.6, 0.0001),  (0.8, 0.0001)] )
 TrainSettings.set_min_batch_size(1)
+TrainSettings.set_test_frequency(100)
+TrainSettings.set_nTest(100000)
+TrainSettings.set_samplesPerStep(200000)
+TrainSettings.set_trainingSteps(100)
 
 ###
 ### 3. Train network and Study results
 ### Comment: For different trainingSetSizes the neural network reset and not saved, hence train and evaluation of yPredicted are done together currently
 ###
+
 xTest, yTest, yPredicted = train_and_test(Generator, Regressor, TrainSettings)
-plot_results("CDF unrandomized deterministic inputs", yPredicted, xTest, yTest, Generator)
+plot_results("predicted vs. expected", yPredicted, xTest, yTest, Generator)