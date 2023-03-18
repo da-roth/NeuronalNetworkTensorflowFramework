021f5e9310e027c36e7090b9d15ce8affbef7c82
Daniel Roth <118249021+da-roth@users.noreply.github.com>
2023-03-18 10:28:12 -0400

    only one training per training step

--- montecarlolearning/vanilla_train_one_epoch.py
+++ montecarlolearning/vanilla_train_one_epoch.py
@@ -7,13 +7,8 @@ def vanilla_train_one_epoch(# training graph from vanilla_training_graph()
     m, n = Regressor.x.shape
     
     # minimization loop over mini-batches
-    first = 0 #index
-    last = min(batch_size, m) #Assume batch size is 256, than first index = 0, after one loop first is at 256. If last is 750 (again index) we will ahve 3 iterations in while
-    while first < m:
-        Regressor.session.run(Regressor.minimizer, feed_dict = {
-            Regressor.inputs: Regressor.x[first:last], 
-            Regressor.labels: Regressor.y[first:last],
+    Regressor.session.run(Regressor.minimizer, feed_dict = {
+            Regressor.inputs: Regressor.x, 
+            Regressor.labels: Regressor.y,
             Regressor.learning_rate: learning_rate
         })
-        first = last
-        last = min(first + batch_size, m)
