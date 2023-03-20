391e31e0f473e038c24e53132c4d54fe25e8bc48
Daniel Roth <118249021+da-roth@users.noreply.github.com>
2023-03-20 08:49:26 -0400

    draft reset graph in neural approximator (not working)

--- montecarlolearning/vanilla_train_one_epoch.py
+++ montecarlolearning/vanilla_train_one_epoch.py
@@ -12,15 +12,16 @@ def vanilla_train_one_epoch(# training graph from vanilla_training_graph()
                             # params, left to client code
                             learning_rate, batch_size):        
     
-    m, n = Regressor.x.shape
+    if isinstance(Regressor.x, tf.Tensor):
+        input_tensor = tf.convert_to_tensor(Regressor.x, dtype=tf.float32)
+        label_tensor = tf.convert_to_tensor(Regressor.y, dtype=tf.float32)
+        Regressor.session.run(Regressor.minimizer,feed_dict = {Regressor.learning_rate: learning_rate})
+    else:
+        m, n = Regressor.x.shape
+        # minimization loop over mini-batches
+        Regressor.session.run(Regressor.minimizer, feed_dict = {
+                Regressor.inputs: Regressor.x, 
+                Regressor.labels: Regressor.y,
+                Regressor.learning_rate: learning_rate
+            })
     
-    input_tensor = tf.convert_to_tensor(Regressor.x, dtype=tf.float32)
-    label_tensor = tf.convert_to_tensor(Regressor.y, dtype=tf.float32)
-
-    # minimization loop over mini-batches
-    # Regressor.session.run(Regressor.minimizer, feed_dict = {
-    #         Regressor.inputs: input_tensor, 
-    #         Regressor.labels: label_tensor,
-    #         Regressor.learning_rate: learning_rate
-    #     })
-    Regressor.session.run(Regressor.minimizer,feed_dict = {Regressor.learning_rate: learning_rate})
