dbce0d854dae92f0da9a503184fe06aa78734dbb
da-roth <118249021+da-roth@users.noreply.github.com>
2023-03-30 22:26:23 +0200

    remove unused tf.convert_to_tensor in vanilla_train_one_epoch

--- montecarlolearning/vanilla_train_one_epoch.py
+++ montecarlolearning/vanilla_train_one_epoch.py
@@ -15,16 +15,7 @@ def vanilla_train_one_epoch(# training graph from vanilla_training_graph()
                             learning_rate, batch_size):        
     
     m, n = Regressor.x.shape
-    
-    input_tensor = tf.convert_to_tensor(Regressor.x, dtype=tf.float32)
-    label_tensor = tf.convert_to_tensor(Regressor.y, dtype=tf.float32)
 
-    # minimization loop over mini-batches
-    # Regressor.session.run(Regressor.minimizer, feed_dict = {
-    #         Regressor.inputs: input_tensor, 
-    #         Regressor.labels: label_tensor,
-    #         Regressor.learning_rate: learning_rate
-    #     })
     if isinstance(Regressor.x_raw, tf.Tensor):
         Regressor.session.run(Regressor.minimizer,feed_dict = {Regressor.learning_rate: learning_rate, Regressor.predictionsTest: np.random.rand(1, Regressor.n), Regressor.isTraining: True})
     else:
