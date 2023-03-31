26b61188b68fc81d4c30272d8a55a87ee0b6b263
Daniel Roth <118249021+da-roth@users.noreply.github.com>
2023-03-31 09:43:33 +0200

    Revert "learning rate re-factor draft"
    This reverts commit 2050e47580621e5cd8fe62605978d9e8471204d3.

--- montecarlolearning/train.py
+++ montecarlolearning/train.py
@@ -54,11 +54,7 @@ def train(description,
     for epoch in range(TrainingSettings.epochs):
         
         # interpolate learning rate in cycle
-        if Regressor._Generator.TrainMethod == TrainingMethod.Standard:
-            learning_rate = np.interp(epoch / TrainingSettings.epochs, lr_schedule_epochs, lr_schedule_rates)
-        elif Regressor._Generator.TrainMethod == TrainingMethod.GenerateDataDuringTraining:
-            learning_rate = np.interp(TrainingSettings.madeSteps / TrainingSettings.TrainingSteps, lr_schedule_epochs, lr_schedule_rates)
-            TrainingSettings.increaseMadeSteps()
+        learning_rate = np.interp(epoch / TrainingSettings.epochs, lr_schedule_epochs, lr_schedule_rates)
         
         # train one epoch
         if not Regressor._Generator.Differential:
