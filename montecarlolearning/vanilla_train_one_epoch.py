14cc61f4fbdc7878a4dae0113b4f0b41658bcb5b
da-roth <118249021+da-roth@users.noreply.github.com>
2023-03-31 10:49:52 +0200

    fix bug in while for dataframe

--- montecarlolearning/vanilla_train_one_epoch.py
+++ montecarlolearning/vanilla_train_one_epoch.py
@@ -21,10 +21,11 @@ def vanilla_train_one_epoch(# training graph from vanilla_training_graph()
     else:
         first = 0 #index
         last = min(batch_size, m) #Assume batch size is 256, than first index = 0, after one loop first is at 256. If last is 750 (again index) we will ahve 3 iterations in while
-        Regressor.session.run(Regressor.minimizer, feed_dict = {
-            Regressor.inputs: Regressor.x[first:last], 
-            Regressor.labels: Regressor.y[first:last],
-            Regressor.learning_rate: learning_rate
-        })
-        first = last
-        last = min(first + batch_size, m)
+        while first < m:
+            Regressor.session.run(Regressor.minimizer, feed_dict = {
+                Regressor.inputs: Regressor.x[first:last], 
+                Regressor.labels: Regressor.y[first:last],
+                Regressor.learning_rate: learning_rate
+            })
+            first = last
+            last = min(first + batch_size, m)
