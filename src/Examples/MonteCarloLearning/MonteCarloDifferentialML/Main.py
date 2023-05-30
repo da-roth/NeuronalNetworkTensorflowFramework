032596d488281f971e1a3416b21c3e09de33da8a
Daniel Roth <118249021+da-roth@users.noreply.github.com>
2023-05-30 12:55:02 +0200

    closed seems to work, added debug main

---
+++ src/Examples/MonteCarloLearning/MonteCarloDifferentialML/Main.py
@@ -0,0 +1,62 @@
+### Copy of Main.py as a juypter notebook to visualize results
+
+###
+### 0. Import packages and references
+###
+### Import framework
+import os
+mainDirectory = os.getcwd()
+packageFile = os.path.abspath(os.path.join(mainDirectory, 'montecarlolearning', 'packages.py'))
+exec(open(packageFile).read())
+
+###
+### 1. Training data
+###
+#from CDF import *
+###
+### 1. Training data
+###
+###
+### 1. Training data Generator/Importer
+###
+
+Generator = GBM_Differential(GBM_Case.ClosedSolution)
+Generator.set_inputName('S')
+Generator.set_outputName('EuropeanCallPrice(S)')
+
+###
+### 2. Set Nueral network structure / Hyperparameters
+### 
+
+Regressor = Neural_Approximator()
+Regressor.set_Generator(Generator)
+Regressor.set_hiddenNeurons(50)
+Regressor.set_hiddenLayers(2)
+Regressor.set_activationFunctionsHidden(tf.nn.sigmoid)
+Regressor.set_activationFunctionOutput(tf.nn.sigmoid)
+Regressor.set_weight_seed(1)
+
+#hiddenNeurons = 20               # we use equal neurons for each hidden layer
+#hiddenLayers = 3                # amount of hidden layers
+#activationFunctionsHidden = [tf.nn.tanh]   # activation functions of hidden layers
+
+
+###
+### 3. Set Nueral network structure / Hyperparameters
+### 
+
+TrainSettings = TrainingSettings()
+TrainSettings.set_learning_rate_schedule([(0.0, 0.001),(0.333, 0.0001),(0.666, 0.0001)])
+TrainSettings.set_min_batch_size(1)
+TrainSettings.set_test_frequency(100)
+TrainSettings.set_nTest(20000)
+TrainSettings.set_samplesPerStep(2000)
+TrainSettings.set_trainingSteps(1000)
+
+
+###
+### 3. Train network and Study results
+### Comment: For different trainingSetSizes the neural network reset and not saved, hence train and evaluation of yPredicted are done together currently
+###
+xTest, yTest, yPredicted = train_and_test(Generator, Regressor, TrainSettings)
+plot_results("predicted vs. expected", yPredicted, xTest, yTest, Generator)
